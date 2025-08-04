c41e6be027c90c4d00ee0bc0f0ca6cc8258673ab
Brandon Blaylock <brandon@null.pub>
2025-08-03 17:29:26 -0700

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A simple flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      shell = with pkgs; mkShell {
+        # Insert Packages Here
+        packages = [ deno jupyter ];
+      };
+    in {
+      devShells.default = shell;
+    });
+}
+
